e850272a435d016baff9a479c0e41be0d099d47c
Jon Phipps <jphipps@madcreek.com>
2015-11-21 12:34:58 -0500

    change mitul to casa

--- src/Casa/Generator/GeneratorServiceProvider.php
+++ src/Casa/Generator/GeneratorServiceProvider.php
@@ -31,27 +31,27 @@ class GeneratorServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton('mitul.generator.publish', function ($app) {
+        $this->app->singleton('casa.generator.publish', function ($app) {
             return new PublisherCommand();
         });
 
-        $this->app->singleton('mitul.generator.api', function ($app) {
+        $this->app->singleton('casa.generator.api', function ($app) {
             return new APIGeneratorCommand();
         });
 
-        $this->app->singleton('mitul.generator.scaffold', function ($app) {
+        $this->app->singleton('casa.generator.scaffold', function ($app) {
             return new ScaffoldGeneratorCommand();
         });
 
-        $this->app->singleton('mitul.generator.scaffold_api', function ($app) {
+        $this->app->singleton('casa.generator.scaffold_api', function ($app) {
             return new ScaffoldAPIGeneratorCommand();
         });
 
         $this->commands([
-            'mitul.generator.publish',
-            'mitul.generator.api',
-            'mitul.generator.scaffold',
-            'mitul.generator.scaffold_api',
+            'casa.generator.publish',
+            'casa.generator.api',
+            'casa.generator.scaffold',
+            'casa.generator.scaffold_api',
         ]);
     }
 }
